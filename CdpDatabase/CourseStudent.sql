926a02afe227c0cf0927eb9acfc136a9f5a840ca
Andrey Melnikov <melnikov.work@gmail.com>
2018-04-12 16:26:45 +0300

    changes

---
+++ CdpDatabase/CourseStudent.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE [dbo].[CourseStudent](
+	[CourseId] [int] NOT NULL,
+	[StudentId] [int] NOT NULL,
+ CONSTRAINT [PK_dbo.CourseStudent] PRIMARY KEY CLUSTERED 
+(
+	[CourseId] ASC,
+	[StudentId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[CourseStudent]  WITH CHECK ADD  CONSTRAINT [FK_dbo.CourseStudent_dbo.Courses_CourseId] FOREIGN KEY([CourseId])
+REFERENCES [dbo].[Courses] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[CourseStudent] CHECK CONSTRAINT [FK_dbo.CourseStudent_dbo.Courses_CourseId]
+GO
+
+ALTER TABLE [dbo].[CourseStudent]  WITH CHECK ADD  CONSTRAINT [FK_dbo.CourseStudent_dbo.Students_StudentId] FOREIGN KEY([StudentId])
+REFERENCES [dbo].[Students] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[CourseStudent] CHECK CONSTRAINT [FK_dbo.CourseStudent_dbo.Students_StudentId]
+GO
+
+
---
+++ CdpDatabase/Purchases.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Purchases](
+	[PurchaseId] [int] IDENTITY(1,1) NOT NULL,
+	[Person] [nvarchar](max) NULL,
+	[Address] [nvarchar](max) NULL,
+	[BookId] [int] NOT NULL,
+	[Date] [datetime] NOT NULL,
+ CONSTRAINT [PK_dbo.Purchases] PRIMARY KEY CLUSTERED 
+(
+	[PurchaseId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
