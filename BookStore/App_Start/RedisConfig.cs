1e3a3a3597a63550275babc32241359058643609
Andrei Melnikau <melnikov.work@gmail.com>
2018-05-19 10:18:44 +0300

    redis cache for search results.

---
+++ BookStore/App_Start/RedisConfig.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace BookStore.App_Start
+{
+    public class RedisConfig
+    {
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            return ConnectionMultiplexer.Connect("redis-epm-cdp.redis.cache.windows.net:6380,password=ikhuzdMZQnfGp3o0Urblkv9nXmygmxE4RhNBhdshZ9M=,ssl=True,abortConnect=False");
+        });
+
+        public static ConnectionMultiplexer RedisConnection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
--- BookStore/Controllers/SearchController.cs
+++ BookStore/Controllers/SearchController.cs
@@ -1,6 +1,8 @@
 ﻿using BookStore.App_Start;
 using BookStore.Models;
 using Microsoft.Azure.Search.Models;
+using Newtonsoft.Json;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
@@ -10,6 +12,7 @@ namespace BookStore.Controllers
     public class SearchController : Controller
     {
         private AzureSearch azureSearch = new AzureSearch();
+        private ConnectionMultiplexer redisConnection = RedisConfig.RedisConnection;
         // GET: Search
         public ActionResult Index()
         {
@@ -24,15 +27,72 @@ namespace BookStore.Controllers
                 query = "*";
             }
 
-            var books = azureSearch.Search(query);
+            var books = GetCachedBooks(query);
 
-            var soccers = azureSearch.SearchSoccers(query);
+            var soccers = GetCachedSoccers(query);
 
-            var students = azureSearch.SearchStudents(query);
+            var students = GetCachedStudents(query);
 
             var output = Tuple.Create(books, soccers, students);
 
             return View(output);
+        }
+
+        private IList<SearchResult<Book>> GetCachedBooks(string query)
+        {
+            IList<SearchResult<Book>> books = null;
+
+            IDatabase cache = redisConnection.GetDatabase();
+            var serializedBooks = cache.StringGet(query+":books");
+            if (!serializedBooks.IsNullOrEmpty)
+            {
+                books = JsonConvert.DeserializeObject<IList<SearchResult<Book>>>(serializedBooks);
+            }
+            else
+            {
+                books = azureSearch.Search(query);
+                cache.StringSet(query+":books", JsonConvert.SerializeObject(books), TimeSpan.FromSeconds(30));
+            }
+
+            return books;
+        }
+
+        private IList<SearchResult<Player>> GetCachedSoccers(string query)
+        {
+            IList<SearchResult<Player>> books = null;
+
+            IDatabase cache = redisConnection.GetDatabase();
+            var serializedBooks = cache.StringGet(query + ":soccers");
+            if (!serializedBooks.IsNullOrEmpty)
+            {
+                books = JsonConvert.DeserializeObject<IList<SearchResult<Player>>>(serializedBooks);
+            }
+            else
+            {
+                books = azureSearch.SearchSoccers(query);
+                cache.StringSet(query + ":soccers", JsonConvert.SerializeObject(books), TimeSpan.FromSeconds(30));
+            }
+
+            return books;
+        }
+
+        private IList<SearchResult<Student>> GetCachedStudents(string query)
+        {
+            IList<SearchResult<Student>> books = null;
+
+            IDatabase cache = redisConnection.GetDatabase();
+            var serializedBooks = cache.StringGet(query + ":students");
+            if (!serializedBooks.IsNullOrEmpty)
+            {
+                books = JsonConvert.DeserializeObject<IList<SearchResult<Student>>>(serializedBooks);
+            }
+            else
+            {
+                books = azureSearch.SearchStudents(query);
+                cache.StringSet(query + ":students", JsonConvert.SerializeObject(books), TimeSpan.FromSeconds(30));
+            }
+
+            return books;
         }
     }
 }
